e84e92f998de5845d8a177cca1dd236c81aa5469
Spike_Projects <msm74588@gmail.com>
2023-10-19 02:24:27 +0530

    testing download js, implement observer pattern

--- src/lib/components/previewWindow.svelte
+++ src/lib/components/previewWindow.svelte
@@ -3,6 +3,7 @@
 	import WindowButton from './windowButton.svelte';
 	import WindowCloseSymbolic from '$lib/AdwIcons/window-close-symbolic.svelte';
 	import DownloadSimple from '$lib/AdwIcons/download-simple.svelte';
+	import  downloadImage  from '$lib/logic/download';
 	/**
 	 * @type {{ selectedImgHighRes: any; isOpenWindow?: boolean; }}
 	 */
@@ -31,9 +32,9 @@
 		<div class="flex flex-row w-full dark:border-[#28292ec4] border-[#28292e5b] border-b-[1.5px] items-center">
 			<div class="flex flex-row w-full justify-start gap-1 py-2">
 				<div class="flex items-center pl-2">
-					<button class="bg-violet-500 h-full rounded py-[6px] px-4 text-white font-bold"
-						>Set background</button
-					>
+					<button class="bg-violet-500 h-full rounded py-[6px] px-4 text-white font-bold" on:click={downloadImage("hello")}
+						
+					>Set background</button>
 				</div>
 				<div class=" flex items-center">
 					<button class="duration-100 hover:bg-[#c4c4c4] rounded font-bold aspect-square h-full flex items-center justify-center">
--- src/lib/logic/download.js
+++ src/lib/logic/download.js
@@ -3,20 +3,8 @@ import { appDataDir, downloadDir } from '@tauri-apps/api/path';
 import { isPermissionGranted, requestPermission, sendNotification } from '@tauri-apps/api/notification';
 
 
-const downloadFolderName = 'PaperClient-Downloads';
-
-const appDirectory = async (/** @type {any} */ full_res) => {
-    const appDir = await appDataDir();
-    console.log(appDir);
-};
-
-async function downloadImage(){
-    let permissionGranted = await isPermissionGranted();
-		if (!permissionGranted) {
-			const permission = await requestPermission();
-			permissionGranted = permission === 'granted';
-            console.debug("Permission granted")
-		}
-        const response = await fetch(full_res);
-		const blob = await response.blob();
+const downloadImage = (msg) => {
+    console.log(msg)
 }
+
+export default downloadImage
