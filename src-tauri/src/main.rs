13a228bcfdaf02c526b8b270d162e0cd82135bff
Spike_Projects <msm74588@gmail.com>
2023-10-19 22:14:03 +0530

    snake case issue solved

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,14 +1,13 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-#[tauri::command]
+#[tauri::command(rename_all = "snake_case")]
 fn set_wallpaper(wallpaper_path: String) {
-  // println!("I was invoked from JS! SET WALLPAPER");
-  // println!("{:?}", wallpaper::get());
-  // println!("I was invoked from JS, with this message: {}", wallpaperPath);
   println!("{:?}", wallpaper::get());
   wallpaper::set_from_path(&wallpaper_path).unwrap();
   wallpaper::set_mode(wallpaper::Mode::Crop).unwrap();
+  let info = os_info::get();
+  println!("OS information: {info}");
 }
 
 fn main() {
